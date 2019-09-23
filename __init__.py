8778e179d52c6c78d76b731776ba84fbe1c752a3
Takeshi Ikuma <tikuma@gmail.com>
2019-09-23 15:33:04 -0500

    Added matlab dir funcs

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 # expose classes in libmat module at the package level
 from .libmat import matfile
 from .libmat import matlab_array
+from .libmat import get_matlab_dir
+from .libmat import set_matlab_dir
